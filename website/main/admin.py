5cdab6b38b1a04624eb348f8898e77f095fe71ab
jan <jan3llam@gmail.com>
2022-10-07 17:53:45 +0300

    added group name to user model

--- website/main/admin.py
+++ website/main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post,UserGroup
-from django.contrib.auth.models import User,Group
+from .models import Post
+from django.contrib.auth.models import User
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
@@ -8,8 +8,22 @@ class PostAdmin(admin.ModelAdmin):
 
 
 
+class UserCustomize(admin.ModelAdmin):
+	def group(self, user):
+		groups = []
+		for group in user.groups.all():
+			groups.append(group.name)
+		return ' '.join(groups)
+
+	group.short_description = 'Groups'
+	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'group')
+
 
 admin.site.register(Post,PostAdmin)
+admin.site.unregister(User)
+admin.site.register(User,UserCustomize)
+
+
 
 
 
