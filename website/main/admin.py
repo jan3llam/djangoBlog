4499792901dd4225b42523a081739082f2f9587b
jan <jan3llam@gmail.com>
2022-10-07 17:28:14 +0300

    final edit before customize admin

--- website/main/admin.py
+++ website/main/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from .models import Post,UserGroup
-from django.contrib.auth.models import User,Group
+from .models import Post
+from django.contrib.auth.models import User
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
 	list_display = ('title','author','description','created_at','updated_at')
-
-
 
 
 admin.site.register(Post,PostAdmin)
